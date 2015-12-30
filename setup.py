358a1a9597ed1032d9e8aaeccf58ac24285c1ca3
Patrick Murphy <git@syzgyn.com>
2015-12-30 01:34:41 -0500

    Fix setup.py for Raspberry Pi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup, Extension
 setup(
 	ext_modules=[
 		Extension('patterns.single.shader', ['patterns/single/shader.c'],
-			extra_compile_args=['-Os', '-funroll-loops', '-ffast-math'],
+			extra_compile_args=['-Os', '-funroll-loops', '-ffast-math', '-std=c99'],
 		),
 	],
 )
