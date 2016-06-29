83dd4dc2a2456d18264635aaade05ca4e6f5a41f
Patrick Murphy <git@syzgyn.com>
2016-06-28 22:18:46 -0400

    Update tent layout generation

--- layouts/make_tent.py
+++ layouts/make_tent.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
-x_spacing = 78 #in
-y_spacing = 29
-z_spacing = -17
+x_spacing = 6.5
+y_spacing = 4
+z_spacing = 1
+
+x_total = 4
+y_total = 4
+
+half_x = (x_spacing * (x_total - 1)) / 2
+half_y = (y_spacing * (y_total - 1)) / 2
 
 lines = []
 
-for x in range(0, 4):
-	for y in range(0, 3):
+for x in range(0, x_total):
+	for y in range(0, y_total):
 		lines.append('	{"point": [%.2f, %.2f, %.2f]}' %
-			(x * x_spacing / 6, 
-			y * y_spacing / 6,
-			z_spacing / 6 if y == 1 else 0))
+			(x * x_spacing - half_x, 
+			y * y_spacing - half_y,
+			z_spacing if y in range(1, y_total - 1) else 0))
 print '[\n' + ',\n'.join(lines) + '\n]'
 
